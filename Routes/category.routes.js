4228efb941b0a22be0a9e592b474351c03374563
HazratAli <hazrat369ali@gmail.com>
2022-11-26 08:02:02 +0600

    category api done

--- Routes/category.routes.js
+++ Routes/category.routes.js
@@ -10,6 +10,7 @@ const router = express.Router();
 
 
 router.get('/list/', getAllCategory);
+
 router.delete('/:id', deleteCategory);
 router.patch('/:id', updateCategory);
 router.post('/', saveCategory);
